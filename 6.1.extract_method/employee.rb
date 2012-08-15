449a894fe6bfd36747367e32d35e6189e1fbb23f
SASAKI, Shunsuke (erukiti) <erukiti@gmail.com>
2012-08-15 18:24:37 +0900

    6.1.extract_method/employee: 色々とネタを追加

--- 6.1.extract_method/employee.rb
+++ 6.1.extract_method/employee.rb
@@ -4,13 +4,15 @@
 class MockEmployee
    attr_reader :name
    attr_reader :payment_base
-   attr_reader :complete_years
+   attr_reader :bonus_factor
+   attr_reader :enter_at
    attr_reader :position
 
-  def initialize(name, payment_base, complete_years, position)
+  def initialize(name, payment_base, bonus_factor, enter_at, position)
     @name = name
     @payment_base = payment_base
-    @complete_years = complete_years
+    @bonus_factor = bonus_factor
+    @enter_at = enter_at
     @position = position
   end
 end
@@ -18,15 +20,15 @@ end
 class MockEmployeeDB
   def initialize
     @member = []
-    @member << MockEmployee.new('alice', 100.0, 5, :none) 
-    @member << MockEmployee.new('bob', 120.0, 10, :section_manager)
-    @member << MockEmployee.new('charlie', 150.0, 2, :group_reader)
-    @member << MockEmployee.new('dave', 200.0, 5, :general_manager)
-    @member << MockEmployee.new('eve', 150.0, 0, :none)
+    @member << MockEmployee.new('alice', 100.0, 2, Time.gm(2007,10,1), :none) 
+    @member << MockEmployee.new('bob', 120.0, 1, Time.gm(2002,9,1), :section_manager)
+    @member << MockEmployee.new('charlie', 150.0, 3, Time.gm(2010,4,1), :group_leader)
+    @member << MockEmployee.new('dave', 200.0, 2, Time.gm(2007,1,1), :general_manager)
+    @member << MockEmployee.new('eve', 150.0, 0, Time.gm(2012,4,1), :none)
   end
 
-  def each(&block)
-    @member.each(&block)
+  def method_missing(method, *args, &block)
+    @member.send(method, *args, &block)
   end
 end
 #ここまでリファクタリング対象外
@@ -44,48 +46,65 @@ class Employee
     result
   end
 
-  def get_payment
+  def get_payment(year, month)
     result = {}
     @employee_db.each do |member|
       payment = member.payment_base
-      # 勤続年数が1年以上ならば給料増額
-      if member.complete_years > 1
-        payment *= 1.1
+
+      # 半年以上勤め上げればボーナスが付く
+      if month + year * 12 >= member.enter_at.month + member.enter_at.year * 12 + 6
+         case member.bonus_factor
+         when 1
+           payment *= 1.1
+         when 2
+           payment *= 1.2
+         when 3
+           payment *= 1.3
+         end
       end
 
       # 役職毎の給料増額
       if member.position == :section_manager
         payment *= 2
-      elsif member.position == :group_reader
+      elsif member.position == :group_leader
         payment *= 1.5
       elsif member.position == :general_manager
         payment *= 10
       end
-      
+
       result[member.name] = payment
     end
     result
   end
 
-  def get_holidays
+  def get_holiday(year, month)
     result = {}
     @employee_db.each do |member|
       holiday = 10
-      # 勤続年数が1年以上ならば休暇日数増加
-      if member.complete_years > 1
-        holiday += member.complete_years
+
+      # 半年以上勤め上げれば休暇日数増加
+      if month + year * 12 >= member.enter_at.month + member.enter_at.year * 12 + 6
+        holiday += (month + year * 12 - (member.enter_at.month + member.enter_at.year * 12)) / 12 + 1
       end
 
       # 役職によっては忙しいので休暇が実質無くなっていく
       if member.position == :section_manager
         holiday -= 5
-      elsif member.position == :group_reader
+      elsif member.position == :group_leader
         holiday -= 2
       elsif member.position == :general_manager
         holiday -= 10
       end
 
       result[member.name] = holiday
+    end
+    result
+  end
+
+  def get_work_month(year, month)
+    result = {}
+    @employee_db.each do |member|
+      result[member.name] = month + year * 12 - (member.enter_at.month + member.enter_at.year * 12)
     end
     result
   end
@@ -96,7 +115,7 @@ class EmployeeView
     @employee = Employee.new
   end
 
-  def view_employee
+  def view_employee(year, month)
     result = ""
     result += "<table>\n"
 
@@ -104,33 +123,35 @@ class EmployeeView
     result += "<th>名前</th>"
     result += "<th>休暇日数</th>"
     result += "<th>給与金額</th>"
+    result += "<th>勤続年数</th>"
     result += "</tr>"
     result += "\n"
 
-    payment = @employee.get_payment
-    holidays = @employee.get_holidays
+    payment = @employee.get_payment(year, month)
+    holiday = @employee.get_holiday(year, month)
+    work_month = @employee.get_work_month(year, month)
 
     # 休暇日数や、給与金額の最小・最大値の取得
     payment_max = nil
     payment_min = nil
-    holidays_max = nil
-    holidays_min = nil
+    holiday_max = nil
+    holiday_min = nil
     @employee.get_members.each do |member|
       payment_max = payment[member] if payment_max == nil || payment[member] > payment_max
       payment_min = payment[member] if payment_min == nil || payment[member] < payment_min
-      holidays_max = holidays[member] if holidays_max == nil || holidays[member] > holidays_max
-      holidays_min = holidays[member] if holidays_min == nil || holidays[member] < holidays_min
+      holiday_max = holiday[member] if holiday_max == nil || holiday[member] > holiday_max
+      holiday_min = holiday[member] if holiday_min == nil || holiday[member] < holiday_min
     end
 
     @employee.get_members.each do |member|
       result += "<tr>"
       result += "<td>#{member}</td>"
-      if holidays[member] == holidays_min
-        result += "<td style=\"color: blue\">#{holidays[member]}</td>"
-      elsif holidays[member] == holidays_max
-        result += "<td style=\"color: green\">#{holidays[member]}</td>"
+      if holiday[member] == holiday_min
+        result += "<td style=\"color: blue\">#{holiday[member]}</td>"
+      elsif holiday[member] == holiday_max
+        result += "<td style=\"color: green\">#{holiday[member]}</td>"
       else
-        result += "<td>#{holidays[member]}</td>"
+        result += "<td>#{holiday[member]}</td>"
       end
 
       if payment[member] == payment_min
@@ -140,6 +161,8 @@ class EmployeeView
       else
         result += "<td>#{payment[member]}</td>"
       end
+
+      result += "<td>#{work_month[member]/12}</td>"
 
       result += "</tr>"
       result += "\n"
--- 6.1.extract_method/test_employee.rb
+++ 6.1.extract_method/test_employee.rb
@@ -20,23 +20,33 @@ class TestEmployee < Test::Unit::TestCase
   end
 
   def test_get_payment
-    payments = @employee.get_payment
+    payments = @employee.get_payment(2012, 8)
     assert_equal(payments.size, 5)
-    assert_equal(payments['alice'], 100.0 * 1.1)         #110
+    assert_equal(payments['alice'], 100.0 * 1.2)         #120
     assert_equal(payments['bob'], 120.0 * 1.1 * 2)       #264
-    assert_equal(payments['charlie'], 150.0 * 1.1 * 1.5) #247.5
-    assert_equal(payments['dave'], 200.0 * 1.1 * 10)     #2200
+    assert_equal(payments['charlie'], 150.0 * 1.3 * 1.5) #292.5
+    assert_equal(payments['dave'], 200.0 * 1.2 * 10)     #2400
     assert_equal(payments['eve'], 150)                   #150
   end
 
-  def test_get_payment
-    holidays = @employee.get_holidays
+  def test_get_holiday
+    holidays = @employee.get_holiday(2012, 8)
     assert_equal(holidays.size, 5)
     assert_equal(holidays['alice'], 10 + 5)       #15
     assert_equal(holidays['bob'], 10 + 10 - 5)    #15
-    assert_equal(holidays['charlie'], 10 + 2 - 2) #10
-    assert_equal(holidays['dave'], 10 + 5 - 10)   #5
+    assert_equal(holidays['charlie'], 10 + 3 - 2) #11
+    assert_equal(holidays['dave'], 10 + 6 - 10)   #6
     assert_equal(holidays['eve'], 10)             #10
+  end
+
+  def test_get_work_month
+    work_month = @employee.get_work_month(2012, 8)
+    assert_equal(work_month.size, 5)
+    assert_equal(work_month['alice'], 58)
+    assert_equal(work_month['bob'], 119)
+    assert_equal(work_month['charlie'], 28)
+    assert_equal(work_month['dave'], 67)
+    assert_equal(work_month['eve'], 4)
   end
 end
 
@@ -46,15 +56,15 @@ class TestEmployeeView < Test::Unit::TestCase
   end
 
   def test_view_eomployee
-    view = @employee_view.view_employee
+    view = @employee_view.view_employee(2012, 8)
     assert_equal(view, <<EOF)
 <table>
-<tr><th>名前</th><th>休暇日数</th><th>給与金額</th></tr>
-<tr><td>alice</td><td style="color: green">15</td><td style="color: red">110.0</td></tr>
-<tr><td>bob</td><td style="color: green">15</td><td>264.0</td></tr>
-<tr><td>charlie</td><td>10</td><td>247.5</td></tr>
-<tr><td>dave</td><td style="color: blue">5</td><td style="color: green">2200.0</td></tr>
-<tr><td>eve</td><td>10</td><td>150.0</td></tr>
+<tr><th>名前</th><th>休暇日数</th><th>給与金額</th><th>勤続年数</th></tr>
+<tr><td>alice</td><td style="color: green">15</td><td style="color: red">120.0</td><td>4</td></tr>
+<tr><td>bob</td><td style="color: green">15</td><td>264.0</td><td>9</td></tr>
+<tr><td>charlie</td><td>11</td><td>292.5</td><td>2</td></tr>
+<tr><td>dave</td><td style="color: blue">6</td><td style="color: green">2400.0</td><td>5</td></tr>
+<tr><td>eve</td><td>10</td><td>150.0</td><td>0</td></tr>
 </table>
 EOF
   end
