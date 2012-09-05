dcc4332f27db2d948e9ea05d3a742964848a50c6
SASAKI, Shunsuke (erukiti) <erukiti@gmail.com>
2012-09-05 18:56:33 +0900

    flow/battle.rb: メソッドの移動

--- flow/battle.rb
+++ flow/battle.rb
@@ -12,31 +12,21 @@ class Character
     @defence = defence
     @element = element
   end
-end
 
-class Battle
-  attr_reader :turn
-
-  def initialize(first, second)
-    @first = first
-    @second = second
-    @turn = 1
-  end
-
-  def atack(atacker, defencer)
-    if atacker.element == :dark || defencer.element == :dark
+  def atack(defencer)
+    if @element == :dark || defencer.element == :dark
         return false
-    elsif atacker.atack > defencer.defence
-      damage = atacker.atack - defencer.defence
+    elsif @atack > defencer.defence
+      damage = @atack - defencer.defence
       is_elemental_bonus = false
 
-      if atacker.element != defencer.element
-        if (atacker.element == :water || defencer.element == :water) && 
-           (atacker.element == :fire || defencer.element == :fire)
+      if @element != defencer.element
+        if (@element == :water || defencer.element == :water) && 
+           (@element == :fire || defencer.element == :fire)
           is_elemental_bonus = true
         end
-        if (atacker.element == :wind || defencer.element == :wind) && 
-           (atacker.element == :earth || defencer.element == :earth)
+        if (@element == :wind || defencer.element == :wind) && 
+           (@element == :earth || defencer.element == :earth)
           is_elemental_bonus = true
         end
       end
@@ -50,11 +40,22 @@ class Battle
     true
   end
 
+end
+
+class Battle
+  attr_reader :turn
+
+  def initialize(first, second)
+    @first = first
+    @second = second
+    @turn = 1
+  end
+
   def start
     loop do
-      return nil if !atack(@first, @second)
+      return nil if !@first.atack(@second)
       return @first if @second.life <= 0
-      return nil if !atack(@second, @first)
+      return nil if !@second.atack(@first)
       return @second if @first.life <= 0
       @turn += 1
     end
